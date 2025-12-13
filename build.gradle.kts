89c7b921951fb2dd945c1d70bd54889c019ff7c8
NotAlexNoyle <alexnoyle@icloud.com>
2025-12-13 13:30:24 -0500

    Update shadow to 8.3.9 and spotless to 8.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 plugins {
     id("java") // Import Java plugin.
     id("java-library") // Import Java Library plugin.
-    id("com.diffplug.spotless") version "7.0.4" // Import Spotless plugin.
-    id("com.gradleup.shadow") version "8.3.6" // Import Shadow plugin.
+    id("com.diffplug.spotless") version "8.1.0" // Import Spotless plugin.
+    id("com.gradleup.shadow") version "8.3.9" // Import Shadow plugin.
     id("checkstyle") // Import Checkstyle plugin.
     eclipse // Import Eclipse plugin.
     id("io.freefair.lombok") version "8.13.1" // Automatic lombok support.
